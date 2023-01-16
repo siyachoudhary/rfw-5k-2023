9fd7e6328f357f7cbee218647a8a2ecc4700c695
Siya Choudhary <67403730+siyachoudhary@users.noreply.github.com>
2023-01-15 23:56:59 -0600

    routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import { MyNav } from './components/MyNav';
 import { Home } from './pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App () {
 
   return (
     <div className = "App">
       <MyNav></MyNav>
-        
-
+      <BrowserRouter>
+        <Routes>
+          <Route path='/rfw-5k-2023/' element={<Home/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
 
   );
