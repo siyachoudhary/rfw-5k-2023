3e6a55bafb6aba64b0c775bfa10e87b7837c2c29
aryan-cs <aryan05g@gmail.com>
2023-05-18 09:36:40 -0500

    roles :)

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -44,34 +44,34 @@ export const Team = () => {
 
             <Card index = "b"
                   name = "Ankit Rath"
-                  role = "Cofounder, Finance & Logistics"
+                  role = "Child Predator"
                   linkedIn = "https://www.linkedin.com/in/ankit-rath-967163249/"
                   email = "https://mail.google.com/mail/u/0/?fs=1&to=rathankit27@gmail.com&tf=cm"/>
 
             <Card index = "c"
                   name = "Aryan Gupta"
-                  role = "Cofounder, Web Dev & Graphic Design"
+                  role = "Milf Hunter"
                   linkedIn = "https://www.linkedin.com/in/aryan-g/"
                   github = "https://github.com/aryan-cs"
                   email = "https://mail.google.com/mail/u/0/?fs=1&to=aryan05g@gmail.com&tf=cm"/>
 
             <Card index = "d"
                   name = "Sid Dayaneni"
-                  role = "Cofounder, Web Dev"
+                  role = "Slave"
                   linkedIn = "https://www.linkedin.com/in/siddharth-dayaneni-025119262/"
                   github = "https://github.com/SidD11111"
                   email = "https://mail.google.com/mail/u/0/?fs=1&to=siddharth.dayaneni@gmail.com&tf=cm"/>
 
             <Card index = "e"
                   name = "Siya Choudhary"
-                  role = "Web Dev"
+                  role = "Fat Kid Kicker"
                   linkedIn = "https://www.linkedin.com/in/siya-choudhary-2b0b89229/"
                   github = "https://github.com/siyachoudhary"
                   email = "https://mail.google.com/mail/u/0/?fs=1&to=siyacanvas@gmail.com&tf=cm"/>
 
             <Card index = "f"
                   name = "Shiven Patel"
-                  role = "Cofounder, Outreach"
+                  role = "Rahul's Trans Daughter"
                   linkedIn = "https://www.linkedin.com/in/shiven-patel-467bb4233/"
                   email = "https://mail.google.com/mail/u/0/?fs=1&to=patelshiven1@gmail.com&tf=cm"/>
 
