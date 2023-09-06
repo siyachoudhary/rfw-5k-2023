8ccaddbaa2f5d9f18c656745511d58f7031ec1ae
Siya Choudhary <Siyacutesiya@gmail.com>
2023-09-06 13:22:42 -0500

    footer complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -32,7 +32,8 @@ export const Footer = () => {
     <footer>
       <div className="container">
         <div className="row">
-          <div className="col-md-6">
+          <div className="col-md-6 footerHeader">
+            <img src = {process.env.PUBLIC_URL + "/assets/rfw.png"} alt = "RFW" className = "footerImg" />
             <p className='footerTitle'>RUN FOR WATER 5K</p>
           </div>
           <div className="col-md-6">
@@ -46,6 +47,50 @@ export const Footer = () => {
             </ul>
           </div>
         </div>
+        <div className='row sponsorsRow'>
+          <div className='col-sm missionSection'>
+            <p className='missionTitle'>MISSION STATEMENT</p>
+            <p className='mission'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi bibendum neque egestas congue. Eu feugiat pretium nibh ipsum consequat nisl vel pretium lectus. Volutpat odio facilisis mauris sit amet massa vitae. A erat nam at lectus urna duis convallis. Nibh venenatis cras sed felis eget velit aliquet. Volutpat est velit egestas dui. Lacus vel facilisis volutpat est velit egestas dui id. Congue quisque egestas diam in arcu cursus euismod quis. Morbi tristique senectus et netus. Sit amet purus gravida quis blandit turpis cursus. In mollis nunc sed id semper risus.</p>
+          </div>
+          <div className='col-sm'>
+          <div style={{"margin-top":15, "fontSize":25}}>
+          <p>THANK YOU TO OUR PARTNERS</p>
+        </div>
+          <div className='row sponsorsRow'>
+            <div className='col-sm'>
+              <a href="#"><img src = {process.env.PUBLIC_URL + "/assets/sponsors/waterProj.png"} alt = "RFW" className = "sponsor" /></a>
+            </div>
+            <div className='col-sm'>
+            <a href="#"><img src = {process.env.PUBLIC_URL + "/assets/sponsors/waterProject.png"} alt = "RFW" className = "sponsor" /></a>
+            </div>
+          </div>
+          <div className='row sponsorsRow'>
+            <div className='col-sm'>
+            <a href="#"><img src = {process.env.PUBLIC_URL + "/assets/sponsors/waterProject.png"} alt = "RFW" className = "sponsor" /></a>
+            </div>
+            <div className='col-sm'>
+            <a href="#"><img src = {process.env.PUBLIC_URL + "/assets/sponsors/waterProj.png"} alt = "RFW" className = "sponsor" /></a>
+            </div>
+          </div>
+          </div>
+        </div>
+        {/* <div style={{"margin-top":15, "fontSize":25}}>
+          <p>THANK YOU TO OUR PARTNERS</p>
+        </div>
+        <div className='row sponsorsRow'>
+          <div className='col-sm'>
+            <a href="#"><img src = {process.env.PUBLIC_URL + "/assets/sponsors/waterProject.png"} alt = "RFW" className = "sponsor" /></a>
+          </div>
+          <div className='col-sm'>
+          <a href="#"><img src = {process.env.PUBLIC_URL + "/assets/sponsors/waterProj.png"} alt = "RFW" className = "sponsor" /></a>
+          </div>
+          <div className='col-sm'>
+          <a href="#"><img src = {process.env.PUBLIC_URL + "/assets/sponsors/waterProject.png"} alt = "RFW" className = "sponsor" /></a>
+          </div>
+          <div className='col-sm'>
+          <a href="#"><img src = {process.env.PUBLIC_URL + "/assets/sponsors/waterProj.png"} alt = "RFW" className = "sponsor" /></a>
+          </div>
+        </div> */}
         <div className='rowBottom'>
             <p>Copyright © 2023 Run For Water</p>
           </div>
