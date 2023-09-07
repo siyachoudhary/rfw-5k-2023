eb8eb9864e7f55ddd62e64bf12010456dfe5e76e
SidD11111 <79270579+SidD11111@users.noreply.github.com>
2023-09-07 18:16:05 -0500

    registration done

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,3 +1,4 @@
+import React, { useRef, useEffect, useState } from "react";
 import './Registration.css'
 import PayPal from "../components/paypal";
 // import {getFirestore, collection, addDoc, doc, setDoc, getDoc, getDocs, deleteDoc, query, orderBy} from 'firebase/firestore';
@@ -5,6 +6,95 @@ import db from './firebase.config.js';
 import "./AllPages.css"
 
 export const Registration = () => {
+    const [submitDisabled, setSubmitDisabled] = useState(true);
+    const filledOut = useRef(false);
+    const [paid, setPaid] = useState(false);
+    const [submitButtonText, setsubmitButtonText] = useState("Submit (Pay and Enter Info First)");
+
+
+
+    async function addRunner(){
+        var firstName = document.getElementsByClassName('form-control')[0].value;
+        var lastName = document.getElementsByClassName('form-control')[1].value;
+        var email = document.getElementsByClassName('form-control')[2].value;
+        var gender = document.getElementsByClassName('form-control')[3].value;
+        var age = document.getElementsByClassName('form-control')[4].value;
+        var size = document.getElementsByClassName('form-control')[5].value;
+    
+        const addRunner = await db.collection('runners').add({
+            firstName : firstName,
+            lastName: lastName,
+            email: email,
+            gender: gender,
+            age: age,
+            size: size
+          });
+    
+        console.log(document.getElementsByClassName('form-control')[3].value);
+    
+        console.log('success!')
+        setSubmitDisabled(true);
+    }
+
+    function checkDisable(){
+        var firstName = document.getElementsByClassName('form-control')[0].value;
+        var lastName = document.getElementsByClassName('form-control')[1].value;
+        var email = document.getElementsByClassName('form-control')[2].value;
+        var gender = document.getElementsByClassName('form-control')[3].value;
+        var age = document.getElementsByClassName('form-control')[4].value;
+        var size = document.getElementsByClassName('form-control')[5].value;
+
+
+        if(firstName != "" && lastName != "" && email != "" && gender != "" && age != "" && size != "" && gender != "Choose..." && age != "Choose..." && size != "Choose..."){
+            filledOut.current = true;
+            // alert(filledOut.current)
+            // alert(firstName + " " + lastName + " " + email + " " + gender + " " + age + " " + size)
+        }
+        else if(firstName == "" || lastName == "" || email == "" || gender == "" || age == "" || size == ""){
+            filledOut.current = false;
+        }
+
+        if(paid && filledOut.current) {
+            setSubmitDisabled(false);
+            setsubmitButtonText("Submit");
+        }
+
+        else if(paid) {
+            setSubmitDisabled(true);
+            setsubmitButtonText("Submit (Enter Info First)");
+        }
+
+        else if(filledOut.current) {
+            setSubmitDisabled(true);
+            setsubmitButtonText("Submit (Pay First)");
+        }
+        else {
+            setSubmitDisabled(true);
+        }
+      }
+
+    document.addEventListener("mousemove", function(event) {
+        checkDisable()
+      });
+
+      document.addEventListener("touchmove", function(event) {
+        checkDisable()
+      });
+
+  
+
+    //   document.onclick= function(event) {
+    //     // Compensate for IE<9's non-standard event model
+    //     //
+    //     if (event===undefined) event= window.event;
+    //     checkDisable();
+    // };
+
+    function getFilledOut(){
+        return filledOut.current;
+    }
+
+    
     return(
         <div className = 'registrationSection'>
             <div className = 'formSection'>
@@ -81,37 +171,14 @@ export const Registration = () => {
                     
                 </form>
 
-                {/* <button type="submit" onClick = {addRunner} className="btn btn-primary">Submit</button> */}
+                <button disabled = {submitDisabled} type="submit" onClick = {addRunner} className="btn btn-primary">{submitButtonText}</button>
 
             </div>
 
             <div className = 'paymentSection'>
-                <PayPal/>
+                <PayPal passChildData={setPaid} getFilledOut = {getFilledOut} passDisabled = {setSubmitDisabled}/>
             </div>
 
         </div>
     );
 }
-
-async function addRunner(){
-    var firstName = document.getElementsByClassName('form-control')[0].value;
-    var lastName = document.getElementsByClassName('form-control')[1].value;
-    var email = document.getElementsByClassName('form-control')[2].value;
-    var gender = document.getElementsByClassName('form-control')[3].value;
-    var age = document.getElementsByClassName('form-control')[4].value;
-    var size = document.getElementsByClassName('form-control')[5].value;
-
-    const addRunner = await db.collection('runners').add({
-        firstName : firstName,
-        lastName: lastName,
-        email: email,
-        gender: gender,
-        age: age,
-        size: size
-      });
-
-    console.log(document.getElementsByClassName('form-control')[3].value);
-
-    console.log('success!')
-}
-
